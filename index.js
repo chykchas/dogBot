83767168c62de0fc9cd21bc2f64d7a8a8f5b9235
Nik <chykchas.mail@gmail.com>
2021-11-07 11:45:25 +0200

    reworked import of modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const TelegramBot = require("node-telegram-bot-api"); // подключаем node-telegram-bot-api
-
+import TelegramBot from "node-telegram-bot-api"; // подключаем node-telegram-bot-api
+import fetch from "node-fetch";
 const token = "2042088420:AAGUIPmmCCpNfvyS6t5uIlePn38LIptzFoA"; // тут токен кторый мы получили от botFather
 
 // включаем самого обота
@@ -33,17 +33,15 @@ bot.on("callback_query", (query) => {
   const chatId = query.message.chat.id;
 
   if (query.data === "morePes") {
-    let picture;
-    request(
-      "https://dog.ceo/api/breeds/image/random",
-      function (error, response, body) {
-        picture = JSON.parse(body).message;
-        bot.sendMessage(chatId, picture, {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((responce) => responce.json())
+      .then((picture) =>
+        bot.sendMessage(chatId, picture.message, {
           reply_markup: {
             inline_keyboard: keyboard,
           },
-        });
-      }
-    );
+        })
+      );
   }
 });
+bot.on("polling_error", console.log);
