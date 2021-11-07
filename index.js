dcd90733caecdc74331303b826a2b902d57da674
Nik <chykchas.mail@gmail.com>
2021-11-07 13:36:56 +0200

    importing bot token, reworked responce code

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import TelegramBot from "node-telegram-bot-api"; // подключаем node-telegram-bot-api
 import axios from "axios";
-const token = "2042088420:AAGUIPmmCCpNfvyS6t5uIlePn38LIptzFoA"; // тут токен кторый мы получили от botFather
+import dotenv from "dotenv"; // тут токен кторый мы получили от botFather
+
+dotenv.config();
 
 // включаем самого обота
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 //конфиг клавиатуры
 const keyboard = [
@@ -36,9 +38,7 @@ bot.on("callback_query", (query) => {
     axios
       .get("https://dog.ceo/api/breeds/image/random")
       .then(function (responce) {
-        console.log(responce.data);
-        let picture = responce.data;
-        bot.sendMessage(chatId, picture.message, {
+        bot.sendMessage(chatId, responce.data.message, {
           reply_markup: {
             inline_keyboard: keyboard,
           },
